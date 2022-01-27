c8caabebf754885579455e696130d4afc0621d1f
Naufan Rusyda Faikar <idmuslimdev@gmail.com>
2022-01-27 18:23:32 +0700

    Add pseudo-dynamic response

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import discord
-import dotenv
 import json
 import os
 import random
 import re
-import requests
+from dotenv import load_dotenv
+from transformers import pipeline
+from transformers import BlenderbotTokenizer
+from transformers import BlenderbotForConditionalGeneration
 
-dotenv.load_dotenv()
+load_dotenv()
 
 client = discord.Client()
 
-greeting_words = []
+sentiment_classifier = pipeline('sentiment-analysis')
+tokenizer = BlenderbotTokenizer.from_pretrained('facebook/blenderbot-400M-distill')
+converse_model = BlenderbotForConditionalGeneration.from_pretrained('facebook/blenderbot-400M-distill')
+
+tag_finder = re.compile('<.*?>')
+
+with open('words.json', 'r') as f:
+    words = json.loads(f.read())
+    greeting_words = words['greeting']
+    encouraging_words = words['encouraging']
+    naugthy_words = words['naugthy']
 
 
 @client.event
 async def on_ready():
-    global greeting_words
-    with open('words.json', 'r') as f:
-        words = json.loads(f.read())
-        greeting_words = words['greeting']
+    print(f'{client.user} has connected to Discord!')
 
 
 @client.event
@@ -31,21 +40,52 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    response = []
+
     # Normalise the message
     msg = message.content.lower()
 
-    response = []
+    # Filter the message
+    msg_words = msg.split()
+    for naughty_word in naugthy_words:
+        if any(naughty_word == word for word in msg_words):
+            # Sensor the message
+            # msg = message.content.replace(naughty_word, len(naughty_word) * '*')
+            # await message.edit(content=msg)
 
-    # Respond to the greeting
-    if msg.startswith(tuple(greeting_words)):
-        greeting_word = random.choice(greeting_words).capitalize()
-        response.append(f'{greeting_word}, {message.author.mention}!')
+            # Warn the user
+            await message.channel.send(f"Hey {message.author.mention}, don't talk badly!")
+            return
+
+    # Classify the message
+    msg_sentiment = sentiment_classifier(msg)
+
+    if msg_sentiment[0]['label'] == 'NEGATIVE' and msg_sentiment[0]['score'] > 0.8:
+        # Respond to the depressing message
+        encouraging_word = random.choice(encouraging_words)
+        encouraging_sentence = encouraging_word.replace('{user}', message.author.mention)
+        response.append(encouraging_sentence)
+
+    else:
+        # Respond to the greeting message
+        if msg.startswith(tuple(greeting_words)):
+            greeting_word = random.choice(greeting_words)
+            greeting_sentence = f'{greeting_word}, {message.author.mention}!'
+            response.append(greeting_sentence)
+
+        # Respond to the message
+        msg_token = tokenizer(msg, return_tensors='pt')
+        msg_reply = converse_model.generate(**msg_token)
+        msg_reply = tokenizer.decode(msg_reply[0])
+        msg_reply = re.sub(tag_finder, '', msg_reply)
+        response.append(msg_reply)
 
     # Provide a help
     # if message.channel.name == 'help':
     #     if 'help' in msg:
     #         response.append("I'm here to help you!")
 
-    await message.channel.send(' '.join(response))
+    if response:
+        await message.channel.send(' '.join(response))
 
 client.run(os.getenv('TOKEN'))
